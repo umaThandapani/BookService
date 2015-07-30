7f95d672baa1a23c21a810fbc8d7eb9b961335c3
umathan13@gmail.com <umathan13@gmail.com>
2015-07-30 15:24:09 -0400

    Added controllers

--- BookService/Models/Author.cs
+++ BookService/Models/Author.cs
@@ -12,7 +12,7 @@ namespace BookService.Models
     {
         public int ID { get; set; }
         [Required]
-        public string Name { get, set;}
+        public string Name { get; set; }
 
 
     }
--- BookService/Models/Book.cs
+++ BookService/Models/Book.cs
@@ -3,9 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using System.ComponentModel.DataAnnotations;
+
 namespace BookService.Models
 {
     public class Book
-    {
+    { // creating variables or declaring properties 
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+        public string Genre { get; set; }
+
+        //Foreign Key
+
+        public int AuthorId { get; set; }
+
+        // Navigation property
+
+        public Author Author { get; set; }
+
+
+
     }
 }
