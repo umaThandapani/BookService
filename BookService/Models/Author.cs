4d275fdfdd9ecc323ec934847942e7f269cbc149
umathan <umathan13@gmail.com>
2015-07-30 13:49:24 -0400

    BookService
    Created a New WEBAPI project

---
+++ BookService/Models/Author.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+// using System.Linq;
+// using System.Web;
+// using System;
+
+
+namespace BookService.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Name { get, set;}
+
+
+    }
+}
